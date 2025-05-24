546d48f85f9ac3445765e22e7aebd448a4d8af0c
sean50311 <seanhu1990@gmail.com>
2025-05-25 01:49:14 +0800

    上傳拉麵店資料到firebase

---
+++ line_bot_backend/upload_ramen_shops.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from datetime import datetime
+import os
+import json
+from firebase_admin import credentials, firestore, initialize_app
+
+key_dict = json.loads(os.environ["FIREBASE_KEY_JSON"])
+cred = credentials.Certificate(key_dict)
+initialize_app(cred)
+db = firestore.client()
+
+# 讀取 JSON 檔案
+with open("merged_ramen_store_info.json", encoding="utf-8") as f:
+    data = json.load(f)
+
+# 每間店寫入 Firestore 的 "ramen_shops" collection
+for item in data:
+    store_name = item.get("name", "unknown").replace("/", "_")
+    doc_ref = db.collection("ramen_shops").document(store_name)
+    doc_ref.set(item)
