35bb0bed4a3365b9547d3c8db0bbf4526a4b2804
sean50311 <seanhu1990@gmail.com>
2025-05-27 01:10:39 +0800

    0527 1110 update

--- frontend/ramen-map/src/js/config.js
+++ frontend/ramen-map/src/js/config.js
@@ -1,2 +1,3 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyDDMiLxT_j5_3CeTyybhDuZOcpNgS25-9s";
+// export const GOOGLE_MAPS_API_KEY = "AIzaSyDDMiLxT_j5_3CeTyybhDuZOcpNgS25-9s";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyAedfCLT1kl-K5Oif9JsM0KYik3sG_cJbI";
 export const GOOGLE_MAPS_MAP_ID = "8a1502730d204ef65f904dec"; 
